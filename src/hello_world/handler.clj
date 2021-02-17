ca48e04c18d0794a42ea86023b75477ba7f9db0d
Andrew Whitehouse <andrew@whitehouse.org.uk>
2021-02-17 20:53:28 +0000

    Update message.

--- src/hello_world/handler.clj
+++ src/hello_world/handler.clj
@@ -4,7 +4,7 @@
             [ring.middleware.defaults :refer [wrap-defaults site-defaults]]))
 
 (defroutes app-routes
-  (GET "/" [] "Hello World")
+  (GET "/" [] "Hello Creatives Workshop!")
   (route/not-found "Not Found"))
 
 (def app
